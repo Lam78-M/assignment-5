cd54677a73c287e7853389fab714c54be541f6d6
Lam272 <ayatlam787@gmail.com>
2026-03-07 12:34:35 +0600

    add index and home

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('login-btn')
+.addEventListener('click',function(){
+ 
+    const userInput = document.getElementById('input-user');
+    const userName = userInput.value
+    console.log(userName);
+
+    const inputPin = document.getElementById('input-pin');
+    const pin =inputPin.value ;
+    console.log(pin)
+
+    if(userName == 'admin' && pin== 'admin123' ){
+        alert('Login Success');
+        window.location.replace('/home.html');
+    }
+    else{
+        alert('Wrong username or password');
+        return;
+    }
+})
